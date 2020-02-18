5cf18b04d7820e6bc25a3eb83db967b2481eb55b
Elisa Nogueira <elisanog05@gmail.com>
2020-02-18 15:49:10 -0300

    Created the new and create actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,14 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save
+    redirect_to task_path(@task)
+  end
 end
