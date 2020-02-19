e980ae8677ba29c93b52e11f8c38b38d2f044812
Elisa Nogueira <elisanog05@gmail.com>
2020-02-19 11:08:22 -0300

    Doing the destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'tasks/:id', to: 'tasks#show', as: :task
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update', as: :update_task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy_task
 end
